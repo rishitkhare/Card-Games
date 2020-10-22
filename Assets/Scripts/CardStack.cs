8f38e6e391cb2128016ce2fec5254bdcb7a7b738
Rishit Khare <kharerishit@gmail.com>
2020-10-22 14:13:58 -0700

    CardStack.cs
    A class that represents any stack of cards. This will be used for players' hands, the deck, and any other stacks of cards that will be used in the Game

---
+++ Assets/Scripts/CardStack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardStack {
+
+    #region Fields and Constructor
+
+    private List<Card> cards;
+    private bool isFaceUp;
+
+    public CardStack(bool isFaceUp) {
+        this.isFaceUp = isFaceUp;
+        cards = new List<Card>();
+    }
+
+    #endregion fields
+
+
+
+    #region Accessors
+
+    /*
+     * NOTE: "GetCard(int index)" method should not exist
+     * in order to prevent accidental duplication; i.e. if the method 
+     * a.AddCardToTop(b.GetCard(0));
+     * was used for CardStacks a & b, the top card from b would
+     * be duplicated and added to stack a.
+    */
+
+    public int GetCardSuit(int index) {
+        return cards[index].suit;
+    }
+
+    public int GetCardNumber(int index) {
+        return cards[index].number;
+    }
+
+    //cards are removed from deck when taken
+    public Card TakeCardAt(int index) {
+        Card myCard = cards[index];
+        cards.RemoveAt(index);
+
+        return myCard;
+    }
+
+    public Card TakeTopCard() { return TakeCardAt(0); }
+    public Card TakeBottomCard() { return TakeCardAt(cards.Count - 1); }
+
+    #endregion
+
+
+
+    #region Mutators
+
+    public void AddCardToTop(Card newCard) {
+        cards.Insert(0, newCard);
+    }
+
+    public void AddCardToBottom(Card newCard) {
+        cards.Add(newCard);
+    }
+
+    #endregion
+}
