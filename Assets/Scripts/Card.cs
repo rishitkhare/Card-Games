38cdbe9a39357c32336d7d7c7485154c6c1670bf
Rishit Khare <kharerishit@gmail.com>
2020-10-22 09:19:36 -0700

    Card.cs
    added simple "Card" class

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+﻿public enum Suit {
+    Spades,
+    Clubs,
+    Hearts,
+    Diamonds
+}
+public enum SpecialNumber {
+    King = 11,
+    Queen = 12,
+    Jack = 13,
+    Ace = 0
+}
+public class Card {
+
+    public int suit{get; set; }
+    public int number{get; set; }
+
+    public Card(int suit, int number) {
+        this.suit = suit;
+        this.number = number;
+    }
+}
