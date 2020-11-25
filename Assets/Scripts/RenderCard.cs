079515b5e8e0c224fc5ab849de561ca24d9b37db
Rishit Khare <kharerishit@gmail.com>
2020-11-25 14:42:26 -0800

    RenderCard Script (unfinished)

---
+++ Assets/Scripts/RenderCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCard : MonoBehaviour {
+    public Suit SuitOfRenderedCard;
+    public Rank RankOfRenderedCard;
+    public int orderInLayer;
+
+    private SpriteRenderer suitRenderer;
+    private SpriteRenderer rankRenderer;
+    private SpriteRenderer cardRenderer;
+
+    void Start() {
+        suitRenderer = transform.Find("Suit").GetComponent<SpriteRenderer>();
+        rankRenderer = transform.Find("Rank").GetComponent<SpriteRenderer>();
+        cardRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void SetCardRenderer(Suit suit, Rank rank) {
+        SuitOfRenderedCard = suit;
+        RankOfRenderedCard = rank;
+    }
+
+    private void UpdateCardRenderer() {
+
+    }
+}
