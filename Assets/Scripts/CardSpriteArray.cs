6fcb08cffdc0a300b925d5dd09a37a7fae92a0b3
Rishit Khare <kharerishit@gmail.com>
2021-01-03 19:38:03 -0800

    Card Renderer accomodates two art systems

--- Assets/Scripts/CardSpriteArray.cs
+++ Assets/Scripts/CardSpriteArray.cs
@@ -1,73 +1,132 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(menuName = "Card Sprite Array")]
 public class CardSpriteArray : ScriptableObject {
+    public bool seperateColorSprite;
+
+    public Sprite Card;
+    public Sprite Back;
+
+    public Sprite Red;
+    public Sprite Black;
+
     public Sprite Spades;
     public Sprite Clubs;
     public Sprite Hearts;
     public Sprite Diamonds;
 
-    public Sprite Two;
-    public Sprite Three;
-    public Sprite Four;
-    public Sprite Five;
-    public Sprite Six;
-    public Sprite Seven;
-    public Sprite Eight;
-    public Sprite Nine;
-    public Sprite Ten;
-    public Sprite Jack;
-    public Sprite Queen;
-    public Sprite King;
-    public Sprite Ace;
+    //Item1 = black, Item2 = red (uses black by default)
+    public Sprite[] Two;
+    public Sprite[] Three;
+    public Sprite[] Four;
+    public Sprite[] Five;
+    public Sprite[] Six;
+    public Sprite[] Seven;
+    public Sprite[] Eight;
+    public Sprite[] Nine;
+    public Sprite[] Ten;
+    public Sprite[] Jack;
+    public Sprite[] Queen;
+    public Sprite[] King;
+    public Sprite[] Ace;
 
-    public Sprite GetSprite(Rank rank) {
-        switch (rank) {
-            case (Rank.Two):
-                return Two;
-            case (Rank.Three):
-                return Three;
-            case (Rank.Four):
-                return Four;
-            case (Rank.Five):
-                return Three;
-            case (Rank.Six):
-                return Six;
-            case (Rank.Seven):
-                return Seven;
-            case (Rank.Eight):
-                return Eight;
-            case (Rank.Nine):
-                return Nine;
-            case (Rank.Ten):
-                return Ten;
-            case (Rank.Jack):
-                return Jack;
-            case (Rank.Queen):
-                return Queen;
-            case (Rank.King):
-                return King;
-            case (Rank.Ace):
-                return Ace;
-            default:
-                //this will never occur unless we add a new Rank
-                return null;
+    public Sprite GetRankSprite(Card card) {
+        if (seperateColorSprite || card.Suit == Suit.Spades || card.Suit == Suit.Clubs) {
+            switch (card.Rank) {
+                case (Rank.Two):
+                    return Two[0];
+                case (Rank.Three):
+                    return Three[0];
+                case (Rank.Four):
+                    return Four[0];
+                case (Rank.Five):
+                    return Three[0];
+                case (Rank.Six):
+                    return Six[0];
+                case (Rank.Seven):
+                    return Seven[0];
+                case (Rank.Eight):
+                    return Eight[0];
+                case (Rank.Nine):
+                    return Nine[0];
+                case (Rank.Ten):
+                    return Ten[0];
+                case (Rank.Jack):
+                    return Jack[0];
+                case (Rank.Queen):
+                    return Queen[0];
+                case (Rank.King):
+                    return King[0];
+                case (Rank.Ace):
+                    return Ace[0];
+                default:
+                    //this will never occur unless we add a new Rank
+                    return null;
+            }
+        }
+        else {
+            switch (card.Rank) {
+                case (Rank.Two):
+                    return Two[1];
+                case (Rank.Three):
+                    return Three[1];
+                case (Rank.Four):
+                    return Four[1];
+                case (Rank.Five):
+                    return Three[1];
+                case (Rank.Six):
+                    return Six[1];
+                case (Rank.Seven):
+                    return Seven[1];
+                case (Rank.Eight):
+                    return Eight[1];
+                case (Rank.Nine):
+                    return Nine[1];
+                case (Rank.Ten):
+                    return Ten[1];
+                case (Rank.Jack):
+                    return Jack[1];
+                case (Rank.Queen):
+                    return Queen[1];
+                case (Rank.King):
+                    return King[1];
+                case (Rank.Ace):
+                    return Ace[1];
+                default:
+                    //this will never occur unless we add a new Rank
+                    return null;
+            }
         }
     }
 
-    public Sprite GetSprite(Suit suit) {
-        switch (suit) {
-            case (Suit.Spades):
-                return Spades;
+    public Sprite GetSuitSprite(Card card) {
+        switch(card.Suit) {
             case (Suit.Clubs):
                 return Clubs;
             case (Suit.Diamonds):
                 return Diamonds;
             case (Suit.Hearts):
                 return Hearts;
+            case (Suit.Spades):
+                return Spades;
             default:
-                //this will never occur unless we add a new Suit
                 return null;
         }
+    }
+
+    public Sprite GetColorSprite(Card card) {
+        if(seperateColorSprite) {
+            if(card.Suit == Suit.Spades || card.Suit == Suit.Clubs) {
+                return Black;
+            }
+            else {
+                return Red;
+            }
+        }
+        else {
+            return null;
+        }
+
     }
 }
--- Assets/Scripts/RenderCard.cs
+++ Assets/Scripts/RenderCard.cs
@@ -6,23 +6,34 @@ public class RenderCard : MonoBehaviour {
     public CardSpriteArray spriteArray;
     public Card renderedCard;
     public int orderInLayer;
-    public bool isFlipped; //TODO: make this
+    public bool isFlipped;
 
 
     private SpriteRenderer suitRenderer;
     private SpriteRenderer rankRenderer;
+    private SpriteRenderer colorRenderer;
     private SpriteRenderer cardRenderer;
 
     void Start() {
         suitRenderer = transform.Find("Suit").GetComponent<SpriteRenderer>();
         rankRenderer = transform.Find("Rank").GetComponent<SpriteRenderer>();
+        colorRenderer = transform.Find("Color").GetComponent<SpriteRenderer>();
         cardRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     private void Update() {
-        //cardRenderer.sprite =
-        suitRenderer.sprite = spriteArray.GetSprite(renderedCard.Suit);
-        rankRenderer.sprite = spriteArray.GetSprite(renderedCard.Rank);
+        if(!isFlipped) {
+            cardRenderer.sprite = spriteArray.Card;
+            suitRenderer.sprite = spriteArray.GetSuitSprite(renderedCard);
+            rankRenderer.sprite = spriteArray.GetRankSprite(renderedCard);
+            colorRenderer.sprite = spriteArray.GetColorSprite(renderedCard);
+        }
+        else {
+            cardRenderer.sprite = spriteArray.Back;
+            suitRenderer.sprite = null;
+            rankRenderer.sprite = null;
+            colorRenderer.sprite = null;
+        }
     }
 
     private void UpdateCardRenderer() {
